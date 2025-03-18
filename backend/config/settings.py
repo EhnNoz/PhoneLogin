b0f6caa7e232812dfca6ec25485c08535f40e6ea
EhnNoz <enorozzadeh@gmail.com>
2025-03-18 13:40:17 +0330

    add python anywhere

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qlmgkx@f&)ffbxmd%zvdzf(lcsasv#sbb_zu^gvv-4vy-5q6y0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
